a74a37f444e1038a35a1c1381a6e7288dc8dc91d
mike castleman <m@mlcastle.net>
2022-03-19 20:27:09 -0400

    bump

--- src/dom.ts
+++ src/dom.ts
@@ -14,11 +14,11 @@ export const getElement = <T extends HTMLElement>(
   return elt;
 };
 
-const modClassList = (op: 'add' | 'remove', ...classNames: string[]) => (
-  id: string
-) => {
-  document.getElementById(id)?.classList[op](...classNames);
-};
+const modClassList =
+  (op: 'add' | 'remove', ...classNames: string[]) =>
+  (id: string) => {
+    document.getElementById(id)?.classList[op](...classNames);
+  };
 
 export const hide = modClassList('add', 'invisible');
 export const show = modClassList('remove', 'invisible');
